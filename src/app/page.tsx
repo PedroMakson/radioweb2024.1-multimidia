c2833be3b030d5d7368f365e58832447ffd56590
PedroMakson <maksonjp080@gmail.com>
2024-09-02 17:42:33 -0300

    Subindo o código da atividade.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,55 @@
 'use client'
 
 import { useContext } from "react";
-import { FaPlay, FaPause } from 'react-icons/fa';
+import { FaPlay, FaPause, FaForward, FaBackward } from 'react-icons/fa';
 import { HomeContext } from "./context/HomeContext";
 
 export default function Home() {
   const { playing, togglePlaying } = useContext(HomeContext);
 
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <div onClick={togglePlaying} style={{ cursor: 'pointer' }} className="flex flex-col items-center">
-        {playing ? (
-          <>
-            <FaPause size={100} />
-            <p className="mt-2 text-xl font-bold">Executando</p>
-          </>
-        ) : (
-          <>
-            <FaPlay size={100} className="text-[50px] text-[tomato]" />
-            <p className="mt-2 text-xl font-bold">Pausado</p>
-          </>
-        )}
+    <main className="flex min-h-screen flex-col items-center justify-center bg-green-600 p-6">
+      {/* Capa da Música */}
+      <div className="relative w-64 h-64 bg-black mb-6 flex items-center justify-center">
+        {/* Imagem Centralizada */}
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0IcEo1kdEZj389e07Rju_S5e5iMsHba1Krw&s"
+          alt="Capa da Música"
+          className="object-contain w-full h-full"
+        />
+      </div>
+
+      {/* Nome da Música e Status */}
+      <h1 className="text-2xl font-bold text-white mb-4">
+        {playing ? 'Executando' : 'Pausado'}
+      </h1>
+
+      {/* Controles */}
+      <div className="flex items-center space-x-4">
+        <button
+          onClick={() => console.log('Anterior')}
+          className="text-white"
+        >
+          <FaBackward size={32} />
+        </button>
+
+        <button
+          onClick={togglePlaying}
+          className="flex items-center justify-center w-16 h-16 bg-green-500 rounded-full shadow-lg"
+        >
+          {playing ? (
+            <FaPause className="text-white" size={32} />
+          ) : (
+            <FaPlay className="text-white" size={32} />
+          )}
+        </button>
+
+        <button
+          onClick={() => console.log('Próxima')}
+          className="text-white"
+        >
+          <FaForward size={32} />
+        </button>
       </div>
     </main>
   );
